46f2f057c913c21212d1c50ec86f096bebe92101
bkonjanovski-novelic <152858297+bkonjanovski-novelic@users.noreply.github.com>
2025-10-20 13:39:29 +0200

    Delete src directory

--- src/main.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    println!("My name is Borjan");
-    println!("My favourite programing language is Python :)");
-    println!("I'm learning Rust in order not be replaced by AI");
-    println!("Pozz Srdjan");
-}
